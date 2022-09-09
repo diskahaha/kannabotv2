ab2cbba018ecdb37e6920300c9521b525f7dc3a5
diskahaha <109210020+diskahaha@users.noreply.github.com>
2022-09-09 17:26:31 +0700

    Update creator.js

--- plugins/creator.js
+++ plugins/creator.js
@@ -20,12 +20,12 @@ let handler  = async (m, { conn, command, args, usedPrefix, DevMode }) => {
 //------------ BIO
 let ppown = await conn.profilePictureUrl(nomorown + '@s.whatsapp.net', 'image').catch(_ => hwaifu[1]) 
 let teksbio = `${htki} *BIODATA* ${htka}
-*ɴᴀᴍᴇ:* ⓊⒸⒽⒾⒽⒶ ⓃⒶⓇⓊⓉⓄ
-*ᴀɢᴇ:* 20ᵗʰ
-*sᴛᴀᴛᴜs:* single 
+*ɴᴀᴍᴇ:* -
+*ᴀɢᴇ:* -
+*sᴛᴀᴛᴜs:* -
 
-*ʙɪʀᴛʜᴅᴀʏ:* 21 Des 2002
-*ᴀᴅᴅʀᴇss:* Indonesia, Jawa Barat, Bandung
+*ʙɪʀᴛʜᴅᴀʏ:* -
+*ᴀᴅᴅʀᴇss:* -
 
 *- - sᴋɪʟʟs: - -* 
 > JavaScript [89.7%]
@@ -45,7 +45,6 @@ const sections = [
 	rows: [
 	    {title: "📱 • Nomor", rowId: ".owner nomor"},
 	{title: "🎨 • Biodata", rowId: ".owner bio"},
-	{title: "🌎 • Script", rowId: ".sc"},
 	]
     },{
 	title: `${htjava} SUPPORT ME –––––––·•`,
